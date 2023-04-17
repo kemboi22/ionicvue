4acc3f104d983dd8ed58d52fdcf305b87b1eaece
kemboi22 <kemboielvis22@gmail.xom>
2023-04-17 15:29:13 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicvue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
